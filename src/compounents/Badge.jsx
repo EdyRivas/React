cbb6d41426e733fd032a160a4f80e3d90c107e74
EdyRivas <alexicho03@gmail.com>
2021-05-24 19:53:11 -0600

    modificacion del badge

--- src/compounents/Badge.jsx
+++ src/compounents/Badge.jsx
@@ -9,27 +9,27 @@ class Badge extends Component{
         <React.Fragment>
             <div className="Badge m-5">
                 <div className="Badge__header">
-                    <img src={HeaderBackground} alt="header_background"/>
+                    <img src={this.props.Header_p} alt="header_background"/>
                 </div>
                 <div className="container mt-5 mb-1 Badge__userInfo">
                     <div className="Badge__userImage">
                         <img src={this.props.picture} alt="Profile_picture" />
                     </div>
-                    <h4 className="text-center"> Juan Velazques <i>24</i></h4>
-                    <p className="text-center">Texas city</p>
+                    <h4 className="text-center"> {this.props.name} <i>{this.props.age}</i></h4>
+                    <p className="text-center">{this.props.city}</p>
                 </div>
                 <div className="Badge__info container pt-3">
                     <div className="row">
                         <div className="col">
-                            <h4 className="text-center fw bold">77k</h4>
+                            <h4 className="text-center fw bold">{this.props.followers}</h4>
                             <p className="text-center">Followers</p>
                         </div>
                         <div className="col">
-                            <h4 className="text-center fw bold">777k</h4>
+                            <h4 className="text-center fw bold">{this.props.likes}</h4>
                             <p className="text-center">Likes</p>
                         </div>
                         <div className="col">
-                            <h4 className="text-center fw bold">7.7k</h4>
+                            <h4 className="text-center fw bold">{this.props.post}</h4>
                             <p className="text-center">Pictures</p>
                         </div>
                     </div>
