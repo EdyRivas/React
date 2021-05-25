55d0c8085cd54920667323a9684260b0c542f13f
EdyRivas <alexicho03@gmail.com>
2021-05-24 20:08:08 -0600

    Cambiando la pagina principal y comentando cosillas

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Layout from "./pages/layout/Layout.jsx"
 import Main from "./pages/main/Main.jsx" 
 import Badge from "./compounents/Badge"
+import NewBadge from "./pages/NewBadge/NewBadge"
 
 function App() {
   return (
     <React.Fragment>
       <Layout>
-        <Main title={"Welcome"}></Main>
-      </Layout>
-      <Badge 
+        <NewBadge></NewBadge>
+      </Layout>      
+    </React.Fragment>
+  );
+}
+{/*<Badge 
         Header_p={"https://fondosmil.com/fondo/50457.jpg"}
         picture={"https://pm1.narvii.com/7678/43da254e801715662b2379bd2ed14b61d7429051r1-736-732v2_00.jpg"}
         name={"Tyra Orlson"}
@@ -18,10 +22,8 @@ function App() {
         followers={"777k"}
         likes={"77k"}
         post={"7k"}
-      ></Badge>
-    </React.Fragment>
-  );
-}
+      ></Badge>*/}
+{/*<Main title={"Welcome"}></Main> */}
 
 export default App;
  
