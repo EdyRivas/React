4061b4a77d56f80a83b334d2f287550e66356163
EdyRivas <alexicho03@gmail.com>
2021-05-24 20:47:46 -0600

    añadiendo default al badge

--- src/pages/NewBadge/NewBadge.jsx
+++ src/pages/NewBadge/NewBadge.jsx
@@ -10,12 +10,12 @@ class NewBadge extends React.Component{
         form:{
             Header_p: "",
             picture: "",
-            name: "Tyra Orlson",
-            age: "25",
-            city: "Bergen Norway",
-            followers: "777k",
-            likes: "77k",
-            post: "7k"
+            name: "",
+            age: "",
+            city: "",
+            followers: "",
+            likes: "",
+            post: ""
         }
     }
     render(){
@@ -27,6 +27,13 @@ class NewBadge extends React.Component{
                         <Badge
                             Header_p = {this.state.form.Header_p || "https://fondosmil.com/fondo/50457.jpg"}
                             picture = {this.state.form.picture || "https://pm1.narvii.com/7678/43da254e801715662b2379bd2ed14b61d7429051r1-736-732v2_00.jpg"}
+                            name ={this.state.form.name || "Tyra Orlson"}
+                            age= {this.state.form.age || "25"}
+                            city ={this.state.form.city || "Bergen Norway"}
+                            followers ={this.state.form.followers || "0"}
+                            likes ={this.state.form.likes || "0"}
+                            post ={this.state.form.post || "0"}
+
 
                         ></Badge>
                     </div>
