d570e441e27704431480832075370abf17273cf2
EdyRivas <alexicho03@gmail.com>
2021-05-26 16:35:24 -0600

    Creacion del login

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -5,35 +5,40 @@ import "./login.css"
 class Login extends React.Component{
     render(){
         return(
-            <React.Fragment>    
-                <div className="Login_f m-5">
-                    <form onSubmit={this.props.onSubmit}>
-                        <div className="form-group mb-2">
-                            <label className="mb-1">
-                                Email
-                            </label>
-                            <input 
-                            type ="text"
-                            className="form-control"
-                            />
-                        </div>
+            <React.Fragment> 
+                <div className="Login_M">  
+                    <div className="Login_f m-5">
+                        <img src="https://image.flaticon.com/icons/png/512/4710/4710741.png" alt="logito" />
+                        <form onSubmit={this.props.onSubmit}>
+                            <div className="form-group mb-2">
+                                <label className="mb-1">
+                                    Email
+                                </label>
+                                <input 
+                                type ="text"
+                                className="form-control"
+                                />
+                            </div>
 
-                        <div className="form-group mb-2">
-                            <label className="mb-1">
-                                Password
-                            </label>
-                            <input 
-                            type ="text"
-                            className="form-control"
-                            />
-                            <button type="submit" className="Submit__button">
-                                save
-                            </button>
-                        </div>
+                            <div className="form-group mb-2">
+                                <label className="mb-1">
+                                    Password
+                                </label>
+                                <input 
+                                type ="text"
+                                className="form-control"
+                                />
+                                <button type="submit" className="Submit__button">
+                                    Login
+                                </button>
+                                <a href="">Forgot your pass?</a>
+                            </div>
 
-                        
-                    </form>
+                            
+                        </form>
+                    </div>
                 </div>
+                
 
             </React.Fragment>
         );
