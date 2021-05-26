c581e0a9077d5f41cdf77255fcfcf839aaf918fc
EdyRivas <alexicho03@gmail.com>
2021-05-25 21:20:13 -0600

    arreglo de error

--- src/compounents/Hero.jsx
+++ src/compounents/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+
 import HeroImage from "../images/Hero_img.jpg"
 import "./Style/Hero.css"
 
@@ -8,6 +9,7 @@ const Hero = props =>{
             <div className="Hero">
                 <div className="Hero__color" style={{height: props.h}}></div>
                 <div className="Hero__img" style={{height: props.h}}>
+                   
                     <img src={HeroImage} alt="hero_image" />
                 </div>
             </div>
