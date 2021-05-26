f0c414a6564e8a074bbc36946ddf2edf07afbc53
EdyRivas <alexicho03@gmail.com>
2021-05-25 21:18:24 -0600

    Estableciedo lo que se mostrara

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom"
 import Layout from "./pages/layout/Layout.jsx"
+import Login from "./pages/login/login"
 import Main from "./pages/main/Main.jsx" 
-import Badge from "./compounents/Badge"
-import NewBadge from "./pages/NewBadge/NewBadge"
+import NewBadge from "./pages/NewBadge/NewBadge"  
 
 function App() {
   return (
-    <React.Fragment>
+    <BrowserRouter>
       <Layout>
-        <NewBadge></NewBadge>
+        <Switch>
+          <Route exact path="/" component={Main}></Route>
+          <Route exact path="/new" component={NewBadge}></Route>
+          <Route exact path="/log" component={Login}></Route>
+        </Switch>
       </Layout>      
-    </React.Fragment>
+    </BrowserRouter>
   );
 }
 export default App;
 
-{/*<Badge 
-        Header_p={"https://fondosmil.com/fondo/50457.jpg"}
-        picture={"https://pm1.narvii.com/7678/43da254e801715662b2379bd2ed14b61d7429051r1-736-732v2_00.jpg"}
-        name={"Tyra Orlson"}
-        age={"25"}
-        city={"Bergen Norway"}
-        followers={"777k"}
-        likes={"77k"}
-        post={"7k"}
-      ></Badge>*/}
-{/*<Main title={"Welcome"}></Main> */}
 
  
