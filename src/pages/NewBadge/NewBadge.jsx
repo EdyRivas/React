3e34d5fe84b2ec3e4f5c603b9f8c2592ffcc6434
EdyRivas <alexicho03@gmail.com>
2021-05-25 21:22:52 -0600

    se añaden unas funciones al badge

--- src/pages/NewBadge/NewBadge.jsx
+++ src/pages/NewBadge/NewBadge.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import "./NewBadge.css"
 import Hero from "../../compounents/Hero"
 import Badge from "../../compounents/Badge"
+import BadgeForm from "../../compounents/BadgeForm"
+import api from "../../libs/api"
 
 class NewBadge extends React.Component{
     state={
@@ -16,30 +18,55 @@ class NewBadge extends React.Component{
             followers: "",
             likes: "",
             post: ""
+        },
+    };
+    handleChange = event=>{
+        this.setState({
+            form:{
+                ...this.state.form,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+    handleSubmit = async (event) =>{
+        event.preventDefault();
+        this.setState({loading:true, error:null})
+        try{
+            await api.badge.create(this.state.form)
+            this.setState({loading:false, error:null})
+            this.props.history.push("/")
+        }catch(error){
+            this.setState({loading:false, error: error})
         }
     }
+
     render(){
         return(
             <React.Fragment>
                 <Hero h={"15vh"}></Hero>
                 <div className="container">
-                    <div className="col">
-                        <Badge
-                            Header_p = {this.state.form.Header_p || "https://fondosmil.com/fondo/50457.jpg"}
-                            picture = {this.state.form.picture || "https://pm1.narvii.com/7678/43da254e801715662b2379bd2ed14b61d7429051r1-736-732v2_00.jpg"}
-                            name ={this.state.form.name || "Tyra Orlson"}
-                            age= {this.state.form.age || "25"}
-                            city ={this.state.form.city || "Bergen Norway"}
-                            followers ={this.state.form.followers || "0"}
-                            likes ={this.state.form.likes || "0"}
-                            post ={this.state.form.post || "0"}
-
-
-                        ></Badge>
+                    <div class="row">
+                        <div className="col-6">
+                            <Badge
+                                Header_p = {this.state.form.Header_p || "https://fondosmil.com/fondo/50457.jpg"}
+                                picture = {this.state.form.picture || "https://pm1.narvii.com/7678/43da254e801715662b2379bd2ed14b61d7429051r1-736-732v2_00.jpg"}
+                                name ={this.state.form.name || "Tyra Orlson"}
+                                age= {this.state.form.age || "25"}
+                                city ={this.state.form.city || "Bergen Norway"}
+                                followers ={this.state.form.followers || "0"}
+                                likes ={this.state.form.likes || "0"}
+                                post ={this.state.form.post || "0"}
+                            ></Badge>
+                        </div>
+                        <div className="col-6">
+                            <BadgeForm 
+                            formValue={this.state.form}
+                            onChange={this.handleChange}
+                            onSubmit={this.handleSubmit}
+                            ></BadgeForm>
+                        </div>
                     </div>
-                    <div className="col">
-
-                    </div>
+                    
                 </div>
             </React.Fragment>
         )
