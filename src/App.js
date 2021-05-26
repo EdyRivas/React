b86b50e4343c5e5ce3da5e6cd05a204a824eeced
EdyRivas <alexicho03@gmail.com>
2021-05-26 16:30:05 -0600

    se añadieron las urls finales

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom"
 import Layout from "./pages/layout/Layout.jsx"
+import Sing_up from './pages/Sign_up/sign_up';
 import Login from "./pages/login/login"
 import Main from "./pages/main/Main.jsx" 
 import NewBadge from "./pages/NewBadge/NewBadge"  
+import Error_4 from './pages/Error.jsx';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route exact path="/" component={Main}></Route>
           <Route exact path="/new" component={NewBadge}></Route>
           <Route exact path="/log" component={Login}></Route>
+          <Route exact path="/sign" component={Sing_up}></Route>
+          <Route exact path="/nani" component={Error_4}></Route>
         </Switch>
       </Layout>      
     </BrowserRouter>
