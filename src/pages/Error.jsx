dcb43ec940bb709ac0841cb4b07c42bcc4a5dcd2
EdyRivas <alexicho03@gmail.com>
2021-05-26 16:37:08 -0600

    se crea la pagina del error 404

---
+++ src/pages/Error.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Error.css"
+class Error_4 extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+                <div className="E_i">
+                    <img className="mt-5" src="https://image.flaticon.com/icons/png/512/564/564772.png" alt="error" />
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+export default Error_4;
