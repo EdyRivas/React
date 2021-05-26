da19dc56023cf5b1503b0acfcd48cefc0f5088f1
EdyRivas <alexicho03@gmail.com>
2021-05-26 17:05:04 -0600

    adding hero to error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import "./Error.css"
+import Hero from "../compounents/Hero"
 class Error_4 extends React.Component{
     render(){
         return(
             <React.Fragment>
+                <Hero h={"15vh"}></Hero>
                 <div className="E_i">
                     <img className="mt-5" src="https://image.flaticon.com/icons/png/512/564/564772.png" alt="error" />
                 </div>
